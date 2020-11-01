fe339f960db3731235a799b090f4d82f3764660e
Jam.zD <jzdavis1212@gmail.com>
2020-11-01 14:14:27 +0000

    add some starter styling to header, nav links and h1

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-      <div>
-          <h1>My Bookcase</h1>
-          <Link to="/">Home</Link>
-          <Link to="/bookcase" className="bookLink">Bookcase</Link>
-          <Link to="/About">About</Link>
-      </div>
+      <header className="header">
+          <h1 id="mainH1">My Bookcase</h1>
+          <nav>
+          <Link to="/">Home&nbsp;</Link>
+          <Link to="/bookcase" className="bookLink">&nbsp;Bookcase</Link>
+          <Link to="/About">&nbsp; About</Link>
+          </nav>
+      </header>
     ); 
 }
 
