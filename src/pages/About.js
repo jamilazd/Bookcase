705c31ce7653af8d487d264eb8792985211fa8fc
Jam.zD <jzdavis1212@gmail.com>
2020-11-04 22:48:53 +0000

    style about component using bootstrap, style <p>, import google font

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,12 @@
 import React from 'react'; 
-import './Style.css'; 
+import './About.css'; 
 
 const About = () => {
     return (
-        <header>
-            <h1>Welcome to the Bookcase Application</h1>
-            <p>
+        <header class="container">
+            <h2>About Page Turners</h2>
+            <hr/>
+            <p class="about">
             The following application was created by Jamila Davis.
             This bookcase app displays a list of books that a user can save to a local bookcase
             Click on the "Add Book " button to add a book to your bookcase. Use the search bar to find new books! 
