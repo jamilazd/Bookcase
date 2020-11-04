81f6102f58d58bcec493ae47c19518ba8bb49ad9
Jam.zD <jzdavis1212@gmail.com>
2020-11-04 22:41:19 +0000

    create header.css to style header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'; 
 import { Link } from 'react-router-dom'; 
+import './Header.css'; 
 
 const Header = () => {
 
   return (
     
     <header>
+      <h1>Page Turners</h1>
+      <div class="card">
+        <div class="card-body">Welcome to Page Turners, a world of books await!</div>
+      </div>
       <ul class="nav nav-tabs nav justify-content-end navbar navbar-expand-lg navbar-light bg-light">
         <nav class="nav-item">
           <Link class="nav-link Home" to="/">Home</Link>
@@ -21,7 +26,11 @@ const Header = () => {
   ); 
 }
 
+<div class="card">
+  <div class="card-body">
+    This is some text within a card body.
+  </div>
+</div>
 
 
 export default Header; 
-
