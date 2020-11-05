fd3a701b8f7075d8279099929c422e52a29d71e4
Jam.zD <jzdavis1212@gmail.com>
2020-11-05 15:03:44 +0000

    add css styling, change padding of .container in book.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,16 @@
 import React from 'react'; 
 import './About.css'; 
+ 
 
 const About = () => {
     return (
         <header class="container">
             <h3>About Page Turners</h3>
             <hr/>
-            <img src="./background" alt="" />
             <p class="about">
-            The following application was created by Jamila Davis.
-            This bookcase app displays a list of books that a user can save to a local bookcase
-            Click on the "Add Book " button to add a book to your bookcase. Use the search bar to find new books! 
+            This app displays a list of books that you can easily add to a bookcase, using the <b>'Add book'</b> button. 
+            Adding books to your bookcase is a handy way of remembering which books you have to read!
+            Search for more books to add, using the search bar.  
             </p>
         </header>
     ); 
