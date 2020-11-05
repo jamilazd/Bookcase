a135ac3a44233a7008b71cc2b237e988c0f6d558
Jam.zD <jzdavis1212@gmail.com>
2020-11-05 14:33:35 +0000

    experiment with different css styling and moving layout of  book elements around

--- src/components/Book.js
+++ src/components/Book.js
@@ -16,17 +16,17 @@ const Book = (props) => {
   
 
   return (
-      <div class="container">
-        <img class="row" src={thumbnail} alt="book cover"/>
-        <h3 id="bookTitle" class="row">{title}</h3>
-        <h4 class="row">{renderAmount()}</h4>
-        <h4 class="row">{authors.length === 1 ? authors[0] : authors.join(', ') }</h4>
-        <p class="row">{description}</p>
-        <button class="btn btn-secondary" onClick={() => props.addBook(title)}>Add Book</button>
-        {/* <button onClick={() => props.removeBook(id)}>Remove Book</button> */}
-          
-      </div>
-    );    
+    <div class="container">
+      <img class="row" src={thumbnail} alt="book cover"/>
+      <h3 class="row" id="bookTitle">{title}</h3>
+      <h4 class="row">{renderAmount()}</h4>
+      <h4 class="row">{authors.length === 1 ? authors[0] : authors.join(', ') }</h4>
+      <p class="row">{description}</p>
+      <button class="row" class="btn btn-secondary" onClick={() => props.addBook(title)}>Add Book</button>
+      {/* <button onClick={() => props.removeBook(id)}>Remove Book</button> */}
+    </div>
+    
+  );    
 }
 
 Book.propTypes = {
