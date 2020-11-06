da91f3481d904d36d50fdbc6bb1efff5a6c09508
Jam.zD <jzdavis1212@gmail.com>
2020-11-06 17:53:08 +0000

    change style of font and position padding

--- src/pages/Bookcase.js
+++ src/pages/Bookcase.js
@@ -8,9 +8,11 @@ const Bookcase = (props) => {
     return (
         <div className="container">
             <div className="row">
-               <h3 className="col"> Welcome to your bookcase! Here you can save a list of books you would like to read. 
+               <h3 className="col"> My Bookcase</h3>
+               <h4 id="bookcaseinfo">
+                   This is your bookcase. Here you can save a list of books you would like to read. 
                    Search for new books to add to your bookcase, using the Search page. 
-               </h3>
+               </h4>
             </div>
             {props.bookcase.map(book => <Book key={book.id} book={book} removeBook={props.removeBook}/>) }
         </div>
@@ -19,3 +21,6 @@ const Bookcase = (props) => {
 }
 
 export default Bookcase;
+
+
+
