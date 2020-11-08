885222dfcab4a6e5778c0ccef96866685954bb0a
Jam.zD <jzdavis1212@gmail.com>
2020-11-08 16:36:47 +0000

    latest styling to bookcase and about pages. Change font size and line height

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,9 +8,10 @@ const About = () => {
             <h3>About Page Turners</h3>
             <hr/>
             <p className="about">
-            This app displays a list of books that you can easily add to a bookcase, using the <b>'Add book'</b> button. 
-            Adding books to your bookcase is a handy way of remembering which books you have to read!
-            Search for more books to add, using the search bar.  
+            This app can be used to add books to your very own Bookcase. Adding books to your 
+            bookcase is a handy way of remembering which books you have to read. 
+            This app includes an initial list of selected books, on the Search page that you can 
+            add to your Bookcase. You can also search for your own books and add these too! 
             </p>
         </header>
     ); 
