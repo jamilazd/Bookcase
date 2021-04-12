c05dc3d8345e62d1f974e6682854325c6c557e07
Jam.zD <jzdavis1212@gmail.com>
2021-04-12 16:39:08 +0100

    style scroll bar color and navbar using bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,24 +5,23 @@ import './Header.css';
 const Header = () => {
 
   return (
-    
-    <header className="header">
-      <div className="card">
-        <div className="card-body">Welcome to Page Turners, a world of books await!</div>
-      </div>
-      <ul className="nav nav-tabs nav justify-content-start navbar navbar-expand-lg">
-        <nav className="nav-item">
-          <Link className="nav-link Home" to="/">Home</Link>
-        </nav>
-        <nav className="nav-item">
-          <Link className="nav-link Search" to="/search">Search</Link>
-        </nav>
-        <nav className="nav-item">
-          <Link className="nav-link About" to="/About">About</Link>
-        </nav>
-      </ul>
-   </header>   
+    <header>
+      <div >
+        <ul id="nav" className="nav nav-tabs nav justify-content-end navbar navbar-expand-lg navbar-light"> 
+          <nav className="nav-item">
+            <Link className="nav-link Home" to="/">Home</Link>
+          </nav>
+          <nav className="nav-item">
+              <Link className="nav-link Search" to="/Search">Search</Link>
+          </nav>
+          <nav className="nav-item">
+            <Link className="nav-link About" to="/About">About</Link>
+          </nav>
+        </ul>
+      </div>  
+    </header> 
   ); 
 }
 
 export default Header; 
+
