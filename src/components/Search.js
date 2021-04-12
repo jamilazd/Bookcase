e2f18403cc6493dffd3f318e0d74f5964978cc9b
Jam.zD <jzdavis1212@gmail.com>
2021-04-12 17:23:21 +0100

    create style sheet for search component and style search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import './Header.css'; 
+import './Search.css'; 
 
 const Search = (props) => {
     const handleSubmit = (event) => {
@@ -8,17 +8,16 @@ const Search = (props) => {
     }
     return(
         
-        <nav id="navBackground" className="container">
-            <form className="row " onSubmit={handleSubmit}>
+        <div id="searchDiv" className="container">
+            <form id="form" className="row" onSubmit={handleSubmit}>
               <input id="searchbox"  className="col form-control-lg"
                 type="text" 
                 value={props.keyword} 
                 onChange={(e) => {props.setKeyword(e.target.value) } } 
               />
-              <button className="btn btn-outline-secondary" type="search">Find Book</button>
+              <button id="searchButton" className="btn btn-outline-secondary" type="search">Find Book</button>
            </form>
-           <p id="searchInfo">Search for books by book title, using the search function above</p>
-        </nav>
+        </div>
        
     ); 
 }
