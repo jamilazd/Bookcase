28850bd3a74be154655cd03cac9de9737e7dc201
Jam.zD <jzdavis1212@gmail.com>
2021-04-12 17:02:29 +0100

    style about page, add image and resize headings and margins

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,21 @@
 import React from 'react'; 
 import './About.css'; 
+import booklady from './booklady.jpeg'; 
  
 
 const About = () => {
     return (
-        <header className="container">
-            <h3>About Page Turners</h3>
+        <div className="container">
+            <h1 id="aboutHead">Page Turners</h1>
             <hr/>
-            <p className="about">
-            This app can be used to save books to a Bookcase. Adding books to your 
-            bookcase is a handy way of remembering which books you have to read. 
-            This app includes an initial list of selected books on the Search page 
-            that you can add to your Bookcase. You can also search for your own books 
-            and add these too, using the <b>'Add Book'</b> button. 
+            <p id="about">
+              Adding books to your Page Turners bookcase is a handy way of remembering which books you have to read. 
+              This app includes an initial list of selected books on the Search page 
+              that you can add to your Bookcase. You can also search for your own books 
+              and add these too, using the <b>'Add Book'</b> button. 
             </p>
-        </header>
+            <img id="booklady" src={booklady}  alt="lady in room of books"/>
+        </div>
     ); 
 }
 
